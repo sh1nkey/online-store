863df1173c104656cd08a565d06e0494859a731e
Alexey Kudelko <flexivanov237@gmail.com>
2023-02-08 12:29:45 +0300

    added two pages

--- store/products/views.py
+++ store/products/views.py
@@ -6,3 +6,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'products/index.html',)
+
+def products(request):
+    return render(request, 'products/products.html',)
