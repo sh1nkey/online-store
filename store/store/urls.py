fbf5471343e521dc314976e684f15c4bfea0bd39
Alexey Kudelko <flexivanov237@gmail.com>
2023-02-09 11:00:56 +0300

    picture in django-admin may be seen if you need

--- store/store/urls.py
+++ store/store/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 
 from products.views import index, products
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,6 @@ urlpatterns = [
     path('products/', products, name='products'),
 
 ]
+
+if settings.DEBUG :
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
