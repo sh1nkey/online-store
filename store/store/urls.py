3ad50ab04ace0033ed0f649e089addd5bc673116
Alexey Kudelko <flexivanov237@gmail.com>
2023-02-10 10:59:14 +0300

    added login and registration page

--- store/store/urls.py
+++ store/store/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from products.views import index, products
 from django.conf.urls.static import static
@@ -23,7 +23,9 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('products/', products, name='products'),
+    path('products/', include('products.urls', namespace='products')),
+    path('users/', include('users.urls', namespace='users')),
+
 
 ]
 
