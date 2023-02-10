4a58e2ff9903e616616ccc48438120db88360592
Alexey Kudelko <flexivanov237@gmail.com>
2023-02-10 11:45:51 +0300

    authorisation added. error 403 featured (i haven't made anything past authentification yet

--- store/users/views.py
+++ store/users/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
-
+from django.contrib import auth
 # Create your views here.
+from users.models import User
+from users.forms import UserLoginForm
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+    else:
+        form = UserLoginForm()
+    context = {'form': UserLoginForm()}
+    return render(request, 'users/login.html', context)
 
 def registration(request):
     return render(request, 'users/registration.html')
