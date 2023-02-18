495d736b3defc30a798fcd9aaaf3d2e1476e7e55
Alexey Kudelko <flexivanov237@gmail.com>
2023-02-18 21:10:18 +0300

    login and logout FBV are now CBV, quantity of code reduced significantly

--- store/users/views.py
+++ store/users/views.py
@@ -1,26 +1,10 @@
-from django.shortcuts import render,HttpResponseRedirect
-from django.contrib import auth
 from django.urls import reverse, reverse_lazy
+from django.contrib.auth.views import LoginView
 
 from users.models import User
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from products.models import Basket
 from django.views.generic.edit import CreateView, UpdateView
-
-def login(request):
-    if request.method == "POST":
-        form = UserLoginForm(data=request.POST)
-        if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
-            user = auth.authenticate(username=username, password=password)
-            if user:
-                auth.login(request, user)
-                return HttpResponseRedirect(reverse('index'))
-    else:
-        form = UserLoginForm()
-    context = {'title' : 'Авторизация', 'form': form}
-    return render(request, 'users/login.html', context)
 
 
 class UserRegistrationView(CreateView):
@@ -35,18 +19,11 @@ class UserRegistrationView(CreateView):
         return context
 
 
-# def registration(request):
-#     if request.method == "POST":
-#         form = UserRegistrationForm(data=request.POST)
-#         if form.is_valid():
-#             form.save()
-#             messages.success(request, "Поздравляем, вы успешно зарегестрировались!")
-#             return HttpResponseRedirect(reverse('users:login'))
-#         else:
-#             form = UserRegistrationForm()
-#     context =  {'title' : 'Регистрация', 'form' : UserRegistrationForm()}
-#     return render(request, 'users/registration.html', context)
 
+class UserLoginView(LoginView):
+    template_name = 'users/login.html'
+    form_class = UserLoginForm
+    success_url = reverse_lazy('index')
 
 class UserProfileView(UpdateView):
     model = User
@@ -63,23 +40,5 @@ class UserProfileView(UpdateView):
         context['baskets'] = Basket.objects.filter(user=self.object)
         return context
 
-# def profile(request):
-#     if request.method == "POST":
-#         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect(reverse('users:profile'))
-#     else:
-#         form = UserProfileForm(instance=request.user)
-#
-#     context = {'title' : 'Профиль',
-#                'form' : form,
-#                'baskets' : Basket.objects.filter(user=request.user)
-#                }
-#     return render(request, 'users/profile.html', context)
 
-
-def logout(request):
-    auth.logout(request)
-    return HttpResponseRedirect(reverse('index'))
 
