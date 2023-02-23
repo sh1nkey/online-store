d3da9eadb2507874dfe52203656020b96b254ab1
Alexey Kudelko <flexivanov237@gmail.com>
2023-02-23 11:49:59 +0300

    simple unittest for main page added

--- store/products/tests.py
+++ store/products/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from http import HTTPStatus
+from django.urls import reverse
 
-# Create your tests here.
+
+
+class IndexViewTestCase(TestCase):
+    def test_view(self):
+        path = reverse('index')
+        response = self.client.get(path)
+        print(response)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Магазин')
+        self.assertEqual(response, 'products/index.html')
--- store/store/settings.py
+++ store/store/settings.py
@@ -174,3 +174,4 @@ SOCIALACCOUNT_PROVIDERS = {
         ],
     }
 }
+
